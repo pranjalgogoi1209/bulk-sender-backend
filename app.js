dbe46bba68974cace1622742aad25f59b17d75a7
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2025-06-29 11:44:02 +0530

    added a get request

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.use(cors());
 app.use(express.json());
 app.use("/email", sendEmailRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
 app.listen(80, () => {
   console.log(`Server is running on port 80`);
 });
